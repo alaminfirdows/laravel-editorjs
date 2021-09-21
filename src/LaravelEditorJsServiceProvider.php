d1a169ca4aa52bddd6b8a06e996b60a52c21f77e
Al-Amin Firdows <alaminfirdows@gmail.com>
2021-09-21 12:14:43 +0600

    Load views and add vendor publish

--- src/LaravelEditorJsServiceProvider.php
+++ src/LaravelEditorJsServiceProvider.php
@@ -23,6 +23,16 @@ class LaravelEditorJsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel_editorjs');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/laravel_editorjs.php' => config_path('laravel_editorjs.php'),
+            ], 'laravel_editorjs-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => $this->app->resourcePath('views/vendor/laravel_editorjs'),
+            ], 'laravel_editorjs-views');
+        }
     }
 }
