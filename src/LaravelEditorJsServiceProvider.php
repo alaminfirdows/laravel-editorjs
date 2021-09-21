a0fde5d135538f64e9178bdc0b9caa5a4401a6dd
Al-Amin Firdows <alaminfirdows@gmail.com>
2021-09-21 11:54:50 +0600

    Register package service provider

---
+++ src/LaravelEditorJsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AlAminFirdows\LaravelEditorjsHtml;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use ThriveDesk\Billing\Providers\EventServiceProvider;
+use ThriveDesk\Billing\Console\Commands\CancelTrialGraceSunscriptions;
+
+class LaravelEditorJsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
