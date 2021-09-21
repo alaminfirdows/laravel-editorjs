ddbf24c0d9da55abc36e6fd7f6c8447b41dd7c7d
Al-Amin Firdows <alaminfirdows@gmail.com>
2021-09-21 14:53:26 +0600

    Add support for paragraph render

--- config/laravel_editorjs.php
+++ config/laravel_editorjs.php
@@ -1,3 +1,14 @@
 <?php
 
-return [];
+return [
+    'config'    => [
+        'tools' => [
+            'paragraph' => [
+                'text'  => [
+                    'type'          => 'string',
+                    'allowedTags'   => 'i,b,a[href],code[class],mark[class]',
+                ],
+            ],
+        ],
+    ],
+];
